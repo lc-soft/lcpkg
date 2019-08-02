ced53719910a1e94f9c608462f5a74ddcfb33faf
Liu Chao <lc-soft@live.cn>
2019-08-02 20:25:05 +0800

    feat(cli-init): use initial values from lcpkg.json

--- src/cli-init.js
+++ src/cli-init.js
@@ -5,44 +5,70 @@ const prompts = require('prompts')
 const userName = require('git-user-name')()
 const userEmail = require('git-user-email')()
 const program = require('commander')
-
-const PKG_FILE = 'lcpkg.json'
+const pkgfile = require('./config').configFileName
 
 const notice =
-`This utility will walk you through creating a ${PKG_FILE} file.
+`This utility will walk you through creating a ${pkgfile} file.
 It only covers the most common items, and tries to guess sensible defaults.
 
 Use \`lcpkg install <pkg>\` afterwards to install a package and
-save it as a dependency in the ${PKG_FILE} file.
+save it as a dependency in the ${pkgfile} file.
 
 Press ^C at any time to quit.`
 
 async function run() {
-  console.log(notice)
+  let aborted = false
+  let defaults = {
+    name: path.basename(process.cwd()),
+    version: '1.0.0',
+    description: '',
+    author: userName ? `${userName}${userEmail ? `<${userEmail}>` : ''}` : '',
+    arch: [],
+    package: {
+      output: 'dist',
+      entry: {
+        include: 'include',
+        lib: 'build/${arch}-${platform}/${mode}',
+        bin: 'build/${arch}-${platform}/${mode}',
+        output: 'dist'
+      }
+    }
+  }
+  const file = path.join(process.cwd(), pkgfile)
 
+  console.log(notice)
+  if (fs.existsSync(file)) {
+    const pkg = defaults.package
+
+    defaults = Object.assign({}, defaults, JSON.parse(fs.readFileSync(file, { encoding: 'utf-8' })))
+    if (!defaults.package) {
+      defaults.package = pkg
+    }
+  }
   const config = await prompts([
     {
       name: 'name',
       type: 'text',
       message: 'package name:',
-      initial: path.basename(process.cwd())
+      initial: defaults.name
     },
     {
       name: 'version',
       type: 'text',
       message: 'version:',
-      initial: '1.0.0'
+      initial: defaults.version
     },
     {
       name: 'description',
       type: 'text',
-      message: 'description:'
+      message: 'description:',
+      initial: defaults.description,
     },
     {
       name: 'author',
       type: 'text',
       message: 'author:',
-      initial: userName ? `${userName}${userEmail ? `<${userEmail}>` : ''}` : ''
+      initial: defaults.author
     },
     {
       name: 'arch',
@@ -61,7 +87,8 @@ async function run() {
           title: 'arm',
           value: 'arm'
         }
-      ]
+      ],
+      initial: defaults.arch
     },
     {
       name: 'platform',
@@ -84,9 +111,17 @@ async function run() {
           title: 'Mac OS',
           value: 'osx'
         }
-      ]
+      ],
+      initial: defaults.platform
     }
-  ])
+  ], {
+    onCancel: () => { aborted = true }
+  })
+
+  if (aborted) {
+    return false
+  }
+
   config.mode = ['debug', 'release']
 
   let result = await prompts({
@@ -94,8 +129,13 @@ async function run() {
     type: 'confirm',
     message: 'Is your software a C or C++ library that needs to be packaged and distributed?',
     initial: true
+  }, {
+    onCancel: () => { aborted = true }
   })
 
+  if (aborted) {
+    return false
+  }
   if (result.pack) {
     const packConfig = await prompts([
       {
@@ -110,7 +150,7 @@ async function run() {
             '\nThese files are in the include directory, so you should enter \'include\'.\n'
           ].join('\n'))
         },
-        initial: 'include'
+        initial: defaults.package.entry.include
       },
       {
         name: 'lib',
@@ -127,7 +167,7 @@ async function run() {
             '\nThe format of the file path is \'build/${arch}-${platform}/${mode}\', so you should enter it.\n'
           ].join('\n'))
         },
-        initial: 'build/${arch}-${platform}/${mode}'
+        initial: defaults.package.entry.lib
       },
       {
         name: 'bin',
@@ -143,13 +183,21 @@ async function run() {
             '\nThe format of the file path is \'build/${arch}-${platform}/${mode}\', so you should enter it.\n'
           ].join('\n'))
         },
-        initial: 'build/${arch}-${platform}/${mode}'
+        initial: defaults.package.entry.bin
+      },
+      {
+        name: 'content',
+        type: 'text',
+        message: 'Where is the content directory?\n' + chalk.reset(
+          'All files in this directory will be copied to the project root after package installation.\n'
+        ),
+        initial: defaults.package.entry.content
       },
       {
         name: 'output',
         type: 'text',
         message: 'Where do you want to output the packaged file?',
-        initial: 'dist'
+        initial: defaults.package.output
       }
     ])
 
@@ -158,12 +206,12 @@ async function run() {
       entry: {
         lib: packConfig.lib,
         bin: packConfig.bin,
+        content: packConfig.content,
         include: packConfig.include
       }
     }
   }
 
-  const file = path.join(process.cwd(), PKG_FILE)
   const content = JSON.stringify(config, null, 2)
 
   result = await prompts({
@@ -177,9 +225,19 @@ async function run() {
         `${chalk.bold('Is this OK?')}`
       ].join('\n')
     }
+  }, {
+    onCancel: () => { aborted = true }
   })
 
-  fs.writeFileSync(file, content)
+  if (result.pass && !aborted) {
+    fs.writeFileSync(file, content)
+    return true
+  }
+  return false
 }
 
-program.action(run).parse(process.argv)
+program.action(async () => {
+  if (!(await run())) {
+    console.log('lcpkg init canceled')
+  }
+}).parse(process.argv)
