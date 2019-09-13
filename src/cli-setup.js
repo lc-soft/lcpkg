00bebbaae1d086e09f1c96da8602f4f610fded0d
Liu Chao <lc-soft@live.cn>
2019-09-13 22:32:08 +0800

    feat: add cli-setup

---
+++ src/cli-setup.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const path = require('path')
+const program = require('commander')
+const homedir = require('os').homedir()
+const { execSync } = require('child_process')
+
+function setup() {
+  const reposDir = path.resolve(homedir, '.lcpkg', 'repos')
+  const vcpkgDir = path.join(reposDir, 'vcpkg')
+  const cmdBootstrap = process.platform === 'win32' ? '.\\bootstrap-vcpkg.bat' : './bootstrap-vcpkg.sh'
+
+  if (!fs.existsSync(reposDir)) {
+    fs.mkdirSync(reposDir, { recursive: true })
+  }
+  if (fs.existsSync(vcpkgDir)) {
+    execSync('git pull', { cwd: vcpkgDir, stdio: 'inherit' })
+  } else {
+    console.log(`downloading vcpkg into ${reposDir}`)
+    execSync('git clone https://github.com/Microsoft/vcpkg.git', { cwd: reposDir, stdio: 'inherit' })
+  }
+  execSync(cmdBootstrap, { cwd: vcpkgDir, stdio: 'inherit' })
+  lcpkg.cfg.set('lcpkg.info', vcpkgDir)
+}
+
+program
+  .action(setup)
+  .parse(process.argv)
