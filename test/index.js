193b07f93500752790aa3976e190412bcfac804a
Liu <lc-soft@live.cn>
2020-10-03 18:11:28 +0800

    test: add test for uninstall command

--- test/index.js
+++ test/index.js
@@ -6,44 +6,40 @@ const assert = require('assert');
 const { exec } = require('child_process');
 const { homedir } = require('os');
 
-function checkFiles(files, cwd, dir) {
-  return files.every((file) => fs.existsSync(path.resolve(cwd, dir, file)));
-}
-
 describe('lcpkg', () => {
   const fixturesDir = path.resolve(__dirname, 'fixtures');
 
-  after(() => {
+  before(() => {
     fs.removeSync(path.resolve(fixturesDir, 'project', 'lcpkg'));
     fs.removeSync(path.resolve(fixturesDir, 'project-no-deps', 'lcpkg'));
   });
 
   describe('install', () => {
     it('should install the package recorded in lcpkg.json', function (done) {
-      const binDir = 'lcpkg/installed/x86-windows/bin';
-      const expectFiles = ['LCUI.dll', 'libpng16.dll'];
       const cwd = path.resolve(fixturesDir, 'project');
+      const libDir = path.resolve(cwd, 'lcpkg/installed/x86-windows/lib');
 
       this.timeout(120000);
       exec('lcpkg install', { cwd }, (err) => {
-        assert.strictEqual(!err, true, 'command should executed successfully');
-        assert.strictEqual(checkFiles(expectFiles, cwd, binDir), true);
+        assert.strictEqual(!err, true, err && err.message);
+        assert.strictEqual(fs.existsSync(libDir), true, `${libDir} should exist`);
+        assert.deepStrictEqual(fs.readdirSync(libDir).sort(), ['LCUI.lib', 'LCUIMain.lib', 'lzma.lib']);
         done();
       });
     });
     it('should install the package specified in the command line arguments', function (done) {
-      const binDir = 'lcpkg/installed/x86-windows/bin';
-      const expectFiles = ['LCUI.dll', 'sqlite3.dll'];
       const cwd = path.resolve(fixturesDir, 'project-no-deps');
+      const binDir = path.resolve(cwd, 'lcpkg/installed/x86-windows/bin');
       const configFile = path.resolve(cwd, 'lcpkg.json');
 
       this.timeout(120000);
       exec('lcpkg install sqlite3 github.com/lc-soft/LCUI', { cwd }, (err) => {
         assert.strictEqual(!err, true, err && err.message);
-        assert.strictEqual(checkFiles(expectFiles, cwd, binDir), true);
         const config = JSON.parse(fs.readFileSync(configFile, 'utf-8'));
         const keys = Object.keys(config.dependencies).sort();
         assert.deepStrictEqual(keys, ['LCUI', 'sqlite3']);
+        assert.strictEqual(fs.existsSync(binDir), true, `${binDir} should exist`);
+        assert.deepStrictEqual(fs.readdirSync(binDir).sort(), ['LCUI.dll', 'LCUI.pdb', 'sqlite3.dll', 'sqlite3.pdb']);
         done();
       });
     });
@@ -68,11 +64,38 @@ describe('lcpkg', () => {
       this.timeout(5000);
       exec('lcpkg link', { cwd }, (err) => {
         assert.strictEqual(!err, true, err && err.message);
-        assert.strictEqual(fs.existsSync(targetDir), true);
-        assert.strictEqual(fs.existsSync(linkDir), true);
-        assert.strictEqual(fs.statSync(linkDir).isDirectory(), true);
-        assert.strictEqual(fs.lstatSync(linkDir).isSymbolicLink(), true);
+        assert.strictEqual(fs.existsSync(targetDir), true, `${targetDir} should exists`);
+        assert.strictEqual(fs.existsSync(linkDir), true, `${linkDir} should exists`);
+        assert.strictEqual(fs.statSync(linkDir).isDirectory(), true, `${linkDir} should be a directory`);
+        assert.strictEqual(fs.lstatSync(linkDir).isSymbolicLink(), true, `${linkDir} should be a symbolic link`);
         assert.strictEqual(fs.readlinkSync(linkDir), targetDir);
+        done();
+      });
+    });
+  });
+  describe('uninstall', () => {
+    it('should remove symlink of current package folder', function (done) {
+      const cwd = path.resolve(fixturesDir, 'project');
+      const linkDir = path.resolve(homedir(), '.lcpkg', 'packages', 'project', '1.0.0');
+
+      this.timeout(5000);
+      exec('lcpkg unlink', { cwd }, (err) => {
+        assert.strictEqual(!err, true, err);
+        assert.strictEqual(fs.existsSync(linkDir), false, `${linkDir} should be removed`);
+        done();
+      });
+    });
+    it('should remove packages', function (done) {
+      this.timeout(5000);
+      const cwd = path.resolve(fixturesDir, 'project-no-deps');
+      const configFile = path.resolve(cwd, 'lcpkg.json');
+
+      this.timeout(120000);
+      exec('lcpkg uninstall sqlite3 LCUI', { cwd }, (err) => {
+        assert.strictEqual(!err, true, err && err.message);
+        const config = JSON.parse(fs.readFileSync(configFile, 'utf-8'));
+        const keys = Object.keys(config.dependencies).sort();
+        assert.deepStrictEqual(keys, []);
         done();
       });
     });
